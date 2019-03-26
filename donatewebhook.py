77c58843c507d7041e826c4f9b8a850f4876ad94
Darian Tavana <datavana@gmail.com>
2019-03-26 02:13:37 +0000

    added dblvoteswebhook

--- donatewebhook.py
+++ donatewebhook.py
@@ -5,9 +5,12 @@ import psycopg2
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = "@@:jrv7b))?7QB:RjQH\hB8"
+VOTES_FOR_REWARD = 20
 conn = psycopg2.connect("dbname=Ticketer user=postgres password=xw!HLUI&$889 host=localhost")
 cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS payments(userid bigint, paymentid varchar);")
+conn.commit()
+cur.execute("CREATE TABLE IF NOT EXISTS votes(userid bigint, count smallint);")
 conn.commit()
 
 @app.route('/api/donatewebhook', methods=['POST'])
@@ -40,6 +43,40 @@ def donatewebhook():
         conn.commit()
         cur.execute("INSERT INTO premiumqueue (userid, guildid, added) VALUES (%s, %s, True);", (userid, guildid))
         conn.commit()
+    abort(404)
+
+@app.route('/api/dblvoteswebhook', methods=['POST'])
+def voteswebhook():
+    if request.headers['Authorization'] != app.config['SECRET_KEY']:
+        abort(404)
+    data = request.form
+    userid = int(data['user'])
+    botid = int(data['bot'])
+    vote_type = data['type']
+    is_weekend = data['isWeekend']
+    if botid == 542709669211275296 and vote_type == "upvote":
+        if is_weekend:
+            vote_add = 2
+        else:
+            vote_add = 1
+        try:
+            cur.execute("INSERT INTO votes (userid, count) VALUES (%s, %s);", (userid, vote_add))
+        except:
+            conn.rollback()
+            cur.execute("UPDATE votes SET count = count + %s WHERE userid = %s;", (vote_add, userid))
+        conn.commit()
+        
+        cur.execute("SELECT count FROM votes WHERE userid = %s;", (userid,))
+        cur_votes = cur.fetchone()
+        cur_votes = cur_votes[0]
+        if cur_votes >= VOTES_FOR_REWARD:
+            cur.execute("INSERT INTO votesqueue (userid, cur_votes, receiveCredit) VALUES (%s, %s, True);", (userid, cur_votes))
+            conn.commit()
+            cur.execute("DELETE FROM votes WHERE userid = %s;", (userid,))
+            conn.commit()
+        else:
+            cur.execute("INSERT INTO votesqueue (userid, cur_votes, receiveCredit) VALUES (%s, %s, False);", (userid, cur_votes))
+            conn.commit()
     abort(404)
 
 if __name__ == "__main__":
